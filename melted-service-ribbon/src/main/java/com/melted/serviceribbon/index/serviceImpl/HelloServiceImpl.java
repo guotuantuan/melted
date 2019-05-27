5095e79b92f0ea29437f8c9b0ab63ff8e1fe7150
DESKTOP-58FBE7K\gtt <894587926@qq.com>
2019-05-27 16:20:56 +0800

    提交zuul

--- melted-service-ribbon/src/main/java/com/melted/serviceribbon/index/serviceImpl/HelloServiceImpl.java
+++ melted-service-ribbon/src/main/java/com/melted/serviceribbon/index/serviceImpl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.melted.serviceribbon.index.serviceImpl;
 
 import com.melted.serviceribbon.index.service.HelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,8 +16,12 @@ public class HelloServiceImpl implements HelloService{
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     @Override
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
